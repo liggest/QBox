53151c46a34c08726ed1859698a61448854520c3
Wfourg <1052820419@qq.com>
2020-05-20 16:25:07 +0800

    可能成功的确定新框位置
    如题

--- QBox_backend/QBox/QBoxCore/Box/boxdata.py
+++ QBox_backend/QBox/QBoxCore/Box/boxdata.py
@@ -77,5 +77,80 @@ def checkDataByFunc(ckdata,cf):
         return True
     else:
         return False
+'''
+可能成功了？目前可以返回一个位置。如果没有合适的位置返回(-1，-1)
+这里想的是新生成的框可以超出屏幕
+如果固定了新框的大小，极有可能无法在屏幕里出现的状况
+这里的位置是框的左上角
+ def GetNewBoxPosition(screensize,boxposition,boxsize,newboxesize):
+    exist = []
+    for i in range(len(boxposition)):
+        tempx = boxposition[i][0] + boxsize[i][0]
+        tempy = boxposition[i][1] + boxsize[i][1]
+        exist.append([boxposition[i][0],tempx,boxposition[i][1],tempy])
+    sw = screensize[0]
+    sh = screensize[1]
 
-     
+    temp = []
+    for i in range(len(boxposition)):
+        countx = 0
+        county = 0
+        while countx < sw and county < sh:
+            if countx < exist[i][0] and county < exist[i][2]:
+                if (countx + newboxesize[0] < exist[i][0] and county + newboxesize[1] < exist[i][2]) \
+                    or (countx + newboxesize[0] < exist[i][0] and county + newboxesize[1] >= exist[i][2]) \
+                        or (countx + newboxesize[0] >= exist[i][0] and county + newboxesize[1] < exist[i][2]):
+                    temp.append([countx,county])
+                    break
+                else:
+                    countx = exist[i][1] + 10
+                    temp.append([countx,county])
+                    break
+            elif (countx >= exist[i][0] and countx <= exist[i][1]) and (county < exist[i][2]):
+                if county + newboxesize[1] < exist[i][2]:
+                    temp.append(countx,county)
+                    break
+                else:
+                    countx = exist[i][1] + 10
+                    temp.append([countx,county])
+                    break
+            elif (countx >= exist[i][0] and countx <= exist[i][1]) and (county >= exist[i][2] and county <= exist[i][3]):
+                countx = exist[i][1] + 10
+                temp.append([countx,county])
+                break
+            elif (countx >= exist[i][0] and countx <= exist[i][1]) and (county > exist[i][3]):
+                temp.append([countx,county])
+                break         
+            elif (countx < exist[i][0]) and (county >= exist[i][2] and county < exist[i][3]):
+                if countx + newboxesize[0] < exist[i][0]:
+                    temp.append([county,county])
+                    break
+                else:
+                    county = exist[i][3] + 10
+                    temp.append([countx,county])
+                    break  
+            elif countx > exist[i][1] and county > exist[i][3]:
+                temp.append([countx,county])
+                break
+            else:
+                temp.append([countx,county])
+                break
+
+    x = -1
+    y = -1
+    #print(temp)
+    for i in range(len(temp)):
+        for j in range(len(exist)):
+            if not ( (temp[i][0] + newboxesize[0] < exist[j][0] and temp[i][1] + newboxesize[1] < exist[j][2]) \
+                or (temp[i][0] > exist[j][1] or temp[i][1] > exist[j][3]) \
+                    or (temp[i][0] + newboxesize[0] < exist[j][0] and temp[i][1] > exist[j][0]) \
+                        or (temp[i][0] > exist[j][0] and temp[i][1] + newboxesize[1] < exist[j][2])):
+                temp[i][0] = -1
+                temp[i][1] = -1
+    for i in range(len(temp)):
+        if temp[i][0] > 0 and temp[i][0] < sw and temp[i][1] < sh:
+            x = temp[i][0]
+            y = temp[i][1]  
+    #print(temp)
+    return x,y
+'''    
